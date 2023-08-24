ec2b81445b33c68cd4c3e8da5662b884dc1b983e
MUHAMMAD SYAHMI BIN ISMAIL <syahmi2003ismail@gmail.com>
2023-08-25 00:01:27 +0800

    CREATE ACCOUNT
    create database and API back-end

---
+++ src/test/java/org/finalecorp/scorelabs/ScorelabsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.finalecorp.scorelabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorelabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
